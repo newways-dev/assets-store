166b309317138f2ff6348eb588afd604b113a869
newways <reactwebdew@gmail.com>
2024-10-20 17:10:14 +0200

    header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
+import Navbar from '@/components/Navbar'
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang='en' className='h-full'>
       <body className={cn('relative h-full font-sans antialiased', inter.className)}>
         <main className='relative flex flex-col min-h-screen'>
+          <Navbar />
           <div className='flex-grow flex-1'>{children}</div>
         </main>
       </body>
